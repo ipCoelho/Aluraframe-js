b6c887f3b4a49745292948ca9af6035e5d7302aa
ipCoelho <ipcoelho.business@gmail.com>
2021-10-27 18:00:12 -0300

    encapsulation done; querySelector changed to '$'

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,33 @@
 class NegociacaoController {
+    
+    constructor() {
+        
+        let $ = document.querySelector.bind(document);
+        /*  $ explanation -> 
+    
+            Assigning a method to a variable...
+            but, there's a problem, whenever a method is assignied to a variable, it loses it property of being a method, and becomes a function.
+    
+            So, in this case the 'document.querySelector' becomes just 'querySelector()' and it does not carry the 'document' and it isn't associated to the document(html) anymore. The property '.this' does not work, but there is a solution that we can use:
+    
+            document.querySelector.bind(document);
+    
+            The method bind() can be used to 'append' a object before the element.
+            So, we are using the querySelector from the object 'document', to keep the object 'document' on the sentence we 'bind' it using the method 'bind()'.
+            
+            bind meaning = tie or fasten (something) tighly.
+        */
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor = $('#valor');
+    }
     adicionar(event) {
         event.preventDefault();
-        alert('Chamei ação no controller');
+        
+        // HTML's elements imports.
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
     }
+
 }
