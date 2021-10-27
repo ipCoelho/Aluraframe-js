6968da129314da8575edaab97fafdfced6943d4b
ipCoelho <ipcoelho.business@gmail.com>
2021-10-27 13:51:22 -0300

    Comments done.

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,11 +1,15 @@
 class Negociacao {
+    // The constructor method is used when the class is invoked.
     constructor(data, quantidade, valor) {
+        // The arguments will be used when the class is invoked, to attribute the properties properly.
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
 
+        // This method is used to freeze a class, in other words, it do not allow to change the properties latter.
         Object.freeze(this);
     }
+    // Getters.
     get data() {
         return new Date(this._data.getTime());
     }
